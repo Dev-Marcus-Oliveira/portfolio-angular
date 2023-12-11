5265dc8fb7c2395c39a65173671696fc14550abf
Marcus <marcusvinicius17031998@gmail.com>
2023-12-11 12:06:34 -0300

    correção db.json

--- src/app/_models/portfolio.service.ts
+++ src/app/_models/portfolio.service.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class PortfolioService {
-  private apiUrl = 'http://localhost:3000/projects';
+  private apiUrl = 'https://portfolio-db-json.vercel.app/projects';
 
   constructor(private http: HttpClient) {}
 
